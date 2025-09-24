20d41679285612ee478b8a8b226aeb30c930b219
aniket <aniket@9766228627@gmail.com>
2025-09-24 17:06:58 +0530

    Added new method

--- src/main/java/com/Aniket/User_service/repository/UserRepository.java
+++ src/main/java/com/Aniket/User_service/repository/UserRepository.java
@@ -4,9 +4,10 @@ import com.Aniket.User_service.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
     User findByEmail(String email);
+    List<User> findAllByEmail(String email);
 }
