d4e3e15691d65057acf81c1e3d5762eacafb8839
aniket <aniket@9766228627@gmail.com>
2025-09-24 17:03:37 +0530

    Added security configurations

--- src/main/java/com/Aniket/User_service/UserServiceApplication.java
+++ src/main/java/com/Aniket/User_service/UserServiceApplication.java
@@ -1,9 +1,18 @@
 package com.Aniket.User_service;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
+@SecurityScheme(
+        name = "BearerAuth",
+        scheme = "Bearer",
+        bearerFormat = "JWT",
+        type = SecuritySchemeType.HTTP,
+        in = SecuritySchemeIn.HEADER
+)
 @SpringBootApplication
 @EnableDiscoveryClient
 public class UserServiceApplication {
